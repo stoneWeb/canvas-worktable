2dd2d09e36e23da59b1da7e06b65e4cdd6ee8358
stoneWeb <dinglei197@gmail.com>
2020-09-19 01:35:07 +0000

    Deploying to gh-pages from  @ 18e360e9b0adbcf83f5f8e4ac62d3e71e862659f 🚀

---
+++ precache-manifest.71ea7b9c1767fd78b809cff976cdfe6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b42dda855a3be9bc63f945b10877673d",
+    "url": "/canvas-worktable/index.html"
+  },
+  {
+    "revision": "2ce4383c4398483284f0",
+    "url": "/canvas-worktable/static/css/main.67c22a48.chunk.css"
+  },
+  {
+    "revision": "ecdbbddb215c44b0305a",
+    "url": "/canvas-worktable/static/js/2.95c743b5.chunk.js"
+  },
+  {
+    "revision": "74549c08325e899dd17d787a1ff374cf",
+    "url": "/canvas-worktable/static/js/2.95c743b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ce4383c4398483284f0",
+    "url": "/canvas-worktable/static/js/main.d02ddc46.chunk.js"
+  },
+  {
+    "revision": "44189c2e6b1928325155",
+    "url": "/canvas-worktable/static/js/runtime-main.393eff8b.js"
+  }
+]);
